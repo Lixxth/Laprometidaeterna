3f1a3b3a62e0f0ea1d2bf9a21be8b260b8f16608
Lixxth <lizbeth78765@gmail.com>
2025-04-05 17:11:24 -0600

    add assets

--- Assets/scripts/CamaraMovimiento.cs
+++ Assets/scripts/CamaraMovimiento.cs
@@ -4,19 +4,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class NewEmptyCSharpScript : MonoBehaviour {
+public class NewEmptyCSharpScript : MonoBehaviour
+{
 
-    void Start()
-    {
-        
+    public Transform target; // El objeto al que la cámara seguirá
+    public float smoothSpeed; // La velocidad de suavizados
+    public Vector2 maxPosition; // La posición máxima de la cámara
+    public Vector2 minPosition; // La posición mínima de la cámara
+
+
+
+    void Start(){
+
     }
 
-    private void Update()
-    {
-        
+    void LateUpdate(){
+        if (transform.position != target.position)
+        {
+            Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
+
+            // Limitar la posición de la cámara
+            targetPosition.x = Mathf.Clamp(targetPosition.x, minPosition.x, maxPosition.x);
+            targetPosition.y = Mathf.Clamp(targetPosition.y, minPosition.y, maxPosition.y);
+
+            transform.position = Vector3.Lerp(transform.position, targetPosition, smoothSpeed);
+        }
     }
-
-
-
-
 }
